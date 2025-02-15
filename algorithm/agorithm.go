8883fbbf04215c0c0bf1280541370185ec388441
Daniel Pfeifer <daniel@pfeifer-mail.de>
2025-02-15 18:20:38 +0100

    remove unused algorithms

--- algorithm/agorithm.go
+++ algorithm/agorithm.go
@@ -3,31 +3,10 @@
 
 package algorithm
 
-func AnyOf[T any](ts []T, fn func(T) bool) bool {
-	for _, t := range ts {
-		if fn(t) {
-			return true
-		}
-	}
-	return false
-}
-
 func Map[T, R any](ts []T, fn func(T) R) []R {
 	r := make([]R, len(ts))
 	for i, t := range ts {
 		r[i] = fn(t)
 	}
 	return r
-}
-
-func MapEx[T, V any](ts []T, fn func(T) (V, error)) ([]V, error) {
-	var err error
-	result := make([]V, len(ts))
-	for i, t := range ts {
-		result[i], err = fn(t)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return result, nil
 }
