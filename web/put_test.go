be60aaeb62907d21e58eb5950ed02c314be307cf
Daniel Pfeifer <daniel@pfeifer-mail.de>
2025-02-16 00:24:30 +0100

    add web tests

---
+++ web/put_test.go
@@ -0,0 +1,88 @@
+// SPDX-FileCopyrightText: 2025 Daniel Pfeifer <daniel@pfeifer-mail.de>
+// SPDX-License-Identifier: ISC
+
+package web
+
+import (
+	"context"
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/purpleKarrot/cdash-proxy/model"
+)
+
+func TestPutGcovTarOK(t *testing.T) {
+	file, _ := os.Open("../gcovtar/testdata/gcov.tbz2")
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodPut, "/submit?type=GcovTar", file)
+	Put(w, r, func(ctx context.Context, job *model.Job) error {
+		return nil
+	})
+	res := w.Result()
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(body) != `{"status":0}` {
+		t.Errorf("unexpected body: %s", string(body))
+	}
+}
+
+func TestPutGcovTarError(t *testing.T) {
+	file, _ := os.Open("../gcovtar/testdata/gcov.tbz2")
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodPut, "/submit?type=GcovTar", file)
+	Put(w, r, func(ctx context.Context, job *model.Job) error {
+		return errors.New("test error")
+	})
+	res := w.Result()
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(body) != `{"error":"test error"}` {
+		t.Errorf("unexpected body: %s", string(body))
+	}
+}
+
+func TestPutXmlOK(t *testing.T) {
+	file, _ := os.Open("../ctestxml/testdata/Configure.xml")
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodPut, "/submit?project=Example&FileName=Configure.xml", file)
+	Put(w, r, func(ctx context.Context, job *model.Job) error {
+		return nil
+	})
+	res := w.Result()
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(body) != `<cdash><status>OK</status><buildId>4e5a4b59fc4badd8ec47227aa4514ba1</buildId></cdash>` {
+		t.Errorf("unexpected body: %s", string(body))
+	}
+}
+
+func TestPutXmlError(t *testing.T) {
+	file, _ := os.Open("../ctestxml/testdata/Configure.xml")
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodPut, "/submit?project=Example&FileName=Configure.xml", file)
+	Put(w, r, func(ctx context.Context, job *model.Job) error {
+		return errors.New("test error")
+	})
+	res := w.Result()
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(body) != `<cdash><status>ERROR</status><message>test error</message></cdash>` {
+		t.Errorf("unexpected body: %s", string(body))
+	}
+}
